aa9d5a591d8c5b111e731e9d545a034082923dc6
jowabels <joachim.suico@gmail.com>
2018-08-14 15:50:44 +0800

    try except to debug exceptions

--- vt-shiz.py
+++ vt-shiz.py
@@ -31,26 +31,29 @@ def main():
             file_sha1 = myline[-1]
 
             #time.sleep(1)
+            try:
+                if file_sha1 not in cache:
+                    params = {"apikey" : mykey, "resource" : "{0}".format(file_sha1)}
+                    response = requests.get("https://www.virustotal.com/vtapi/v2/file/report", params=params)
+                    if response.status_code == 200:
+                        json_response = response.json()
 
-            if file_sha1 not in cache:
-                params = {"apikey" : mykey, "resource" : "{0}".format(file_sha1)}
-                response = requests.get("https://www.virustotal.com/vtapi/v2/file/report", params=params)
-                if response.status_code == 200:
-                    json_response = response.json()
-
-                    if json_response["response_code"]:
-                        print "{0},{1},{2},{3}/{4}\n".format(file_path, file_sha1, json_response["scan_date"], json_response["positives"], json_response["total"])
-                        out.write("{0},{1},{2},{3}/{4}\n".format(file_path, file_sha1, json_response["scan_date"], json_response["positives"], json_response["total"]))
-                        cache[file_sha1] = json_response
+                        if json_response["response_code"]:
+                            print "{0},{1},{2},{3}/{4}\n".format(file_path, file_sha1, json_response["scan_date"], json_response["positives"], json_response["total"])
+                            out.write("{0},{1},{2},{3}/{4}\n".format(file_path, file_sha1, json_response["scan_date"], json_response["positives"], json_response["total"]))
+                            cache[file_sha1] = json_response
+                        else:
+                            print "{0},{1},NONE,NONE\n".format(file_path, file_sha1)
+                            out.write("{0},{1},NONE,NONE\n".format(file_path, file_sha1))
                     else:
-                        print "{0},{1},NONE,NONE\n".format(file_path, file_sha1)
-                        out.write("{0},{1},NONE,NONE\n".format(file_path, file_sha1))
+                        print "Request HTTP code: {0}".format(response.status_code)
+                        pass
                 else:
-                    print "Request HTTP code: {0}".format(response.status_code)
-                    pass
-            else:
-                print "{0},{1},{2},{3}/{4}\n".format(file_path, file_sha1, cache[file_sha1]["positives"], cache[file_sha1]["total"])
-                out.write("{0},{1},{2},{3}/{4}\n".format(file_path, file_sha1, cache[file_sha1]["positives"], cache[file_sha1]["total"]))
+                    print "{0},{1},{2},{3}/{4}\n".format(file_path, file_sha1, cache[file_sha1]["positives"], cache[file_sha1]["total"])
+                    out.write("{0},{1},{2},{3}/{4}\n".format(file_path, file_sha1, cache[file_sha1]["positives"], cache[file_sha1]["total"]))
+            except Exception as e:
+                print e
+                pass
 
     outjson = "{0}\{1}_json".format(os.path.dirname(hash_file), os.path.basename(hash_file))
     with open(outjson, "w") as oj:
