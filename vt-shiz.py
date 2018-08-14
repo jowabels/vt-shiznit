6289e110eb8536f3cadcc82e911c39846deb2734
jowabels <joachim.suico@gmail.com>
2018-08-14 10:15:52 +0800

    basic handling of non-200 http responses

--- vt-shiz.py
+++ vt-shiz.py
@@ -31,14 +31,18 @@ def main():
 
             params = {"apikey" : mykey, "resource" : "{0}".format(file_sha1)}
             response = requests.get("https://www.virustotal.com/vtapi/v2/file/report", params=params)
-            json_response = response.json()
+            if response.status_code == 200:
+                json_response = response.json()
 
-            if json_response["response_code"]:
-                print "{0},{1},{2},{3}/{4}\n".format(file_path, file_sha1, json_response["scan_date"], json_response["positives"], json_response["total"])
-                out.write("{0},{1},{2},{3}/{4}\n".format(file_path, file_sha1, json_response["scan_date"], json_response["positives"], json_response["total"]))
+                if json_response["response_code"]:
+                    print "{0},{1},{2},{3}/{4}\n".format(file_path, file_sha1, json_response["scan_date"], json_response["positives"], json_response["total"])
+                    out.write("{0},{1},{2},{3}/{4}\n".format(file_path, file_sha1, json_response["scan_date"], json_response["positives"], json_response["total"]))
+                else:
+                    print "{0},{1},NONE,NONE,NONE\n".format(file_path, file_sha1)
+                    out.write("{0},{1},NONE,NONE,NONE\n".format(file_path, file_sha1))
             else:
-                print "{0},{1},NONE,NONE,NONE\n".format(file_path, file_sha1)
-                out.write("{0},{1},NONE,NONE,NONE\n".format(file_path, file_sha1))
+                print "Request HTTP code: {0}".format(response.status_code)
+                pass
 
 
 if __name__ == "__main__":
